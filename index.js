0f753a8988a9e451a5568832f2b4c43abd42f32f
craig stevens <c_stevens44@hotmail.com>
2018-01-12 03:56:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,3 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper(string) {
-  return string.tolowerCase()
-}
-
-function logshout(string) {
-  console.log(string.toUpperCase())
-}
