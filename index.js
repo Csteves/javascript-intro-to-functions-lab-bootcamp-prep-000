1d00f109f12075a8a13ca9389214dbd69a6cc21d
craig stevens <c_stevens44@hotmail.com>
2018-01-12 04:32:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,12 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-if (string.toLowerCase()=== string)
+if (string.toLowerCase() === string)
 return "I can't hear you!"
 
-if (string.toUpperCase()=== string)
+if (string.toUpperCase() === string)
 return "YES INDEED!"
+
+if (string.console.log === "I love you, Grandma.")
+return "I love you, too."
 }
